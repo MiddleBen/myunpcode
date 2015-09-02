5b5a508b6b0b3bfa271c388181f03eaa464afa8b
ben01.li <ben01.li@vipshop.com>
2015-09-02 11:11:22 +0800

    first mmap
    first mmap

--- IO/mmap.c
+++ IO/mmap.c
@@ -12,23 +12,42 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <stdio.h>
+#include <sys/stat.h>
 #define CREATE_MODE S_IRWXU | S_IRWXG
-void err_quite(char *msg) {
+void error_quite(char *msg) {
 	fprintf(stderr, "error: %s, %s\n", msg, strerror(errno));
 	exit(1);
 }
 int main(int argc, char **argv) {
 	if (argc != 2) {
-			err_quite("usage: ./mmap <filepath>");
+		error_quite("usage: ./mmap <filepath>");
 	}
-	int filefd = open(argv[1], O_RDWR, CREATE_MODE);
-	if (filefd < 0) {
-		err_quite("open file error");
+	char *addr, *in;
+	off_t filesize;
+	struct stat filestat;
+	int fd = open(argv[1], O_RDWR);
+	if (fd < 0) {
+		error_quite("open error");
 	}
-	mmap() {
-
+	if (fstat(fd, &filestat) != 0) {
+		error_quite("fstat error");
 	}
+	filesize = filestat.st_size;
+	printf("filesize: %ld\n", filesize);
+	addr = mmap(NULL, filesize, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+	if (addr == MAP_FAILED) {
+		error_quite("mmap error");
+	}
+	close(fd);
+	printf("content: %s\n", addr);
+	addr[20] = '*';
+	if (msync(addr, filesize, MS_SYNC) != 0) {
+		error_quite("msync error!");
+	}
+	if (munmap(addr, filesize) < 0) {
+		error_quite("munmap error");
+	}
+	exit(EXIT_SUCCESS);
 }
-
-
 
